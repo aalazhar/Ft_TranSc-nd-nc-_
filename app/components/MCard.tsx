5bc2e0dacd361b241fa4584e67baa8d29203f100
Anas Alazhari <aalazhar@e1r3p6.1337.ma>
2023-11-03 03:43:29 +0100

    Add new props to render

--- app/components/MCard.tsx
+++ app/components/MCard.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
 
-function MCard() {
+type Props = {
+    login: string
+}
+
+function MCard({login}:Props) {
   return (
 
         <div className='flex h-32 xMedium:h-40 w-40 m-auto'>
             <div className='flex flex-col'>
                 <Image className='rounded-full h-16 w-16  medium:h-28 medium:w-28' alt='' src={'/gsus.jpeg'} height={150} width={150}/>
-                <div className='text-center text-slate-500 medium:text-2xl'>name</div>
+                <div className='text-center text-slate-500 medium:text-2xl'>{login}</div>
             </div>
             <div className='m-auto medium:text-2xl pl-4'>29</div>
         </div>
--- app/components/MatchHist.tsx
+++ app/components/MatchHist.tsx
@@ -2,12 +2,16 @@ import Image from 'next/image'
 import React from 'react'
 import MCard from './MCard'
 import CardM from './CardM'
+import { useSelector } from 'react-redux'
+import { RootState } from '../store/store'
 
 function MatchHist() {
+
+  const {entity} = useSelector((state: RootState) => state.user);
   return (
     <div className='flex min-w-[300px] w-[85%] h-32 justify-between m-auto'>
         <div>
-          <MCard/>
+          <MCard login={entity?.userData?.login}/>
         </div>
           <div className='text-center m-auto text-xl medium:text-2xl'>vs</div>
         <div>
