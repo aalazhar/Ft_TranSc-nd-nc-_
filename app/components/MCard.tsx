71fc83983dd778849395e6b4b87689228a247ee9
Anas Alazhari <aalazhar@e1r2p7.1337.ma>
2023-11-07 05:20:30 +0100

    no change

--- app/components/MCard.tsx
+++ app/components/MCard.tsx
@@ -5,13 +5,13 @@ type Props = {
     login: string
 }
 
-function MCard({login}:Props) {
+function MCard() {
   return (
 
         <div className='flex h-32 xMedium:h-40 w-40 m-auto'>
             <div className='flex flex-col'>
                 <Image className='rounded-full h-16 w-16  medium:h-28 medium:w-28' alt='' src={'/gsus.jpeg'} height={150} width={150}/>
-                <div className='text-center text-slate-500 medium:text-2xl'>{login}</div>
+                <div className='text-center text-slate-500 medium:text-2xl'>{""}</div>
             </div>
             <div className='m-auto medium:text-2xl pl-4'>29</div>
         </div>
