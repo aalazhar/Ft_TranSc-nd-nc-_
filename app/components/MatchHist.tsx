c4c323a3165c72c5c6728fab18639177c9572ef8
Anas Alazhari <aalazhar@e1r2p7.1337.ma>
2023-11-07 05:14:49 +0100

    fix some type and props error

--- app/components/MatchHist.tsx
+++ app/components/MatchHist.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from 'next/image'
 import React from 'react'
 import MCard from './MCard'
@@ -5,13 +6,63 @@ import CardM from './CardM'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store/store'
 
+
+// export interface UserInfos {
+//   id: number;
+//   firstName: string;
+//   lastName: string;
+//   login: string;
+//   ran?: number;
+//   level?: number;
+//   avatar: string;
+// }
+
+// export interface matchHistory {
+//   title: string;
+//   icon: string;
+//   unlocked:boolean;
+// }
+
+// export interface Achievement {
+//   id: number;
+//   name: string;
+//   userName: string;
+//   rank: number;
+//   level: number;
+//   avatar: string;
+// }
+
+
+
+// export interface userState {
+//   userData:UserInfos;
+//   Achiev:Achievement[];
+//   matches:matchHistory[];
+//   loading: boolean;
+//   error: string | null;
+// }
+
+// export const initialState: userState = {
+//   userData: {
+//     id: 0,
+//     login: '',
+//     firstName: '',
+//     lastName: '',
+//     avatar: '',
+//   },
+//   Achiev: [],
+//   matches: [],
+//   loading: false,
+//   error: null,
+// }
+
 function MatchHist() {
 
-  const {entity} = useSelector((state: RootState) => state.user);
+  const {user_Data} = useSelector((state: RootState) => state.user);
   return (
     <div className='flex min-w-[300px] w-[85%] h-32 justify-between m-auto'>
         <div>
-          <MCard login={entity?.userData?.login}/>
+          <MCard/>
         </div>
           <div className='text-center m-auto text-xl medium:text-2xl'>vs</div>
         <div>
